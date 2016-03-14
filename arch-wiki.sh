019ff908b9b6326c51851373834770216da053a5
deadhead420 <deadhead3492@gmail.com>
2016-03-14 16:10:33 -0400

    Arch wiki update

--- arch-wiki.sh
+++ arch-wiki.sh
@@ -7,14 +7,10 @@ if [ "--help" == "$1" ] || [ "-h" == "$1" ]; then
 	echo
 	echo "  where archwiki page name is title of page on wiki.archlinux.org"
 	echo
-	echo "Options:"
-	echo "  -l --language - useage: $this --language <langhere> <search args>"
-	echo "  -u --update  - useage: $this --update (fetches new script from github)"
-	echo
 	echo "Examples:"
 	echo "  $this ssh"
 	echo "  $this beginners guide"
-	echo "  $this --language Italiano the arch way"
+	echo "  $this the arch way"
 	echo
 	exit 0
 fi
@@ -23,8 +19,7 @@ if [ -n "$BROWSER" ]; then run_browser=$BROWSER
 else BROWSER=lynx int=1
 	until [ -n "$run_browser" ]
 	 do
-		which $BROWSER &>/dev/null
-		if [ "$?" -eq "0" ]; then run_browser=$BROWSER
+		if (which $BROWSER &>/dev/null); then run_browser=$BROWSER
 		elif [ "$int" -eq "1" ]; then BROWSER=elinks
 		elif [ "$int" -eq "2" ]; then BROWSER=links
 		else
@@ -36,9 +31,6 @@ else BROWSER=lynx int=1
 fi
 
 query="$*"  # get all params into single query string
-if [ "--language" == "$1" ] || [ "-l" == "$1" ]; then # check for language
-	query=$(echo $query | cut -d " " -f2- | sed "s/^$2 \(.*\)/\u\1 $2/" | sed "s/$2/($2)/g")
-fi
 query=${query// /_}  # substitute spaces with underscores in the query string
 
 # load ArchWiki page with automatic redirect to the correct URL:
